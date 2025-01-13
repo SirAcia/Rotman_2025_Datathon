784de432dcdc1cd10e3505eb6025a32ff1394f52
Mausam <mvadakka@uwo.ca>
2025-01-13 13:14:15 -0500

    metadata

--- Datathon_script.R
+++ Datathon_script.R
@@ -14,5 +14,10 @@ library(readxl)
 # Importing and cleaning data ----------------------------------------------------------------------------------------
 
 #read raw data and metdata files
-raw_data <- read_excel("Datathon_data-2025-Raw.xlsx")
-metadata <- read_excel("Datathon_data-2025-Raw Metadata.xlsx")
+raw_data <- read_excel("Datathon_data-2025-Raw.xlsx", sheet = 1)
+series_meta <- read_excel("Datathon_data-2025-Raw.xlsx", sheet = 2)
+country_meta <- read_excel("Datathon_data-2025-Raw Metadata.xlsx", sheet = 2)
+country.series_meta <- read_excel("Datathon_data-2025-Raw Metadata.xlsx", sheet = 3)
+series.time_meta <- read_excel("Datathon_data-2025-Raw Metadata.xlsx", sheet = 4)
+footnote_meta <- read_excel("Datathon_data-2025-Raw Metadata.xlsx", sheet = 5)
+
