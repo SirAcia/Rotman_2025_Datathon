d3a9035d01fcd4bf45a0d0d081cc0dc56706443b
Mausam <mvadakka@uwo.ca>
2025-01-13 12:46:28 -0500

    read raw file

---
+++ Datathon_script.R
@@ -0,0 +1,17 @@
+#Rotman Datathon 2025
+# Libraries ----------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(funModeling)
+library(Hmisc)
+library(dplyr)
+library(ggpubr)
+library(tidyr)
+library(knitr)
+library(kableExtra)
+library(readxl)
+
+# Importing and cleaning data ----------------------------------------------------------------------------------------
+
+#read csv
+raw_data <- read_excel("/Users/mausamvk/Desktop/Rotman 2025 Datathon Dataset/Datathon_data-2025-Raw.xlsx")
