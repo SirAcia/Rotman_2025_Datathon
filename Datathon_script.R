e0cf4df54516e1fbc6c6e5ca005b57fde5b3799c
SirAcia <122296053+SirAcia@users.noreply.github.com>
2025-01-14 18:01:00 -0500

    Added selected variables

--- Datathon_script.R
+++ Datathon_script.R
@@ -25,58 +25,162 @@ footnote_meta <- read_excel("Datathon_data-2025-Raw Metadata.xlsx", sheet = 5)
 # Renaming the relevant columns
 data <- raw_data %>%
   rename(
+    # Country Identifiers 
     country_name = "Country Name",
     country_code = "Country Code",
+    
+    # Time identifiers 
     year = "Time",
     time_code = "Time Code",
-    cpi_FP.CPI.TOTL = "Consumer price index (2010 = 100) [FP.CPI.TOTL]",
-    net_nat_income_NY.ADJ.NNTY.CD = "Adjusted net national income (current US$) [NY.ADJ.NNTY.CD]",
+    
+    # Cost of living indicators
+    # Inflationary pressure 
+    inflation = "Inflation, consumer prices (annual %) [FP.CPI.TOTL.ZG]",
+    inflation_deflator = "Inflation, GDP deflator (annual %) [NY.GDP.DEFL.KD.ZG]",
+    
+    # Cost of living index (indirect measure)
     net_nat_income_percapita_NY.ADJ.NNTY.PC.CD = "Adjusted net national income per capita (current US$) [NY.ADJ.NNTY.PC.CD]",
-    net_nat_income_percent_NY.ADJ.NNTY.KD.ZG = "Adjusted net national income (annual % growth) [NY.ADJ.NNTY.KD.ZG]",
+    net_nat_income_NY.ADJ.NNTY.CD = "Adjusted net national income (current US$) [NY.ADJ.NNTY.CD]",
+    household_expenditure_GDP = "Households and NPISHs final consumption expenditure (% of GDP) [NE.CON.PRVT.ZS]",
+    household_expenditure_per_capita = "Households and NPISHs Final consumption expenditure per capita growth (annual %) [NE.CON.PRVT.PC.KD.ZG]",
+    household_expenditure_ppp ="Households and NPISHs Final consumption expenditure, PPP (current international $) [NE.CON.PRVT.PP.CD]",
+    
+    # Economic indicators (part of cost of living, measures income)
     gdp_percapita_NY.GDP.PCAP.PP.CD = "GDP per capita, PPP (current international $) [NY.GDP.PCAP.PP.CD]",
     gdp_percapita_growth_NY.GDP.PCAP.KD.ZG = "GDP per capita growth (annual %) [NY.GDP.PCAP.KD.ZG]",
     gdp_deflator_NY.GDP.DEFL.ZS = "GDP deflator (base year varies by country) [NY.GDP.DEFL.ZS]",
-    gdp_NY.GDP.MKTP.CD = "GDP (current US$) [NY.GDP.MKTP.CD]",
-    gdp_constant_NY.GDP.MKTP.KD = "GDP (constant 2015 US$) [NY.GDP.MKTP.KD]",
-    business_startup_cost_IC.REG.COST.PC.ZS = "Cost of business start-up procedures (% of GNI per capita) [IC.REG.COST.PC.ZS]",
-    good_service_expense_GC.XPN.GSRV.ZS = "Goods and services expense (% of expense) [GC.XPN.GSRV.ZS]",
-    household_npish_expenditure_NE.CON.PRVT.KD.ZG = "Households and NPISHs Final consumption expenditure (annual % growth) [NE.CON.PRVT.KD.ZG]",
-    household_npish_expenditure_percapita_NE.CON.PRVT.PC.KD.ZG = "Households and NPISHs Final consumption expenditure per capita growth (annual %) [NE.CON.PRVT.PC.KD.ZG]",
-    part_time_employment_SL.TLF.PART.ZS = "Part time employment, total (% of total employment) [SL.TLF.PART.ZS]",
-    final_consumption_expenditure_NE.CON.TOTL.ZS = "Final consumption expenditure (% of GDP) [NE.CON.TOTL.ZS]",
-    net_primary_income_NY.GSR.NFCY.CD = "Net primary income (Net income from abroad) (current US$) [NY.GSR.NFCY.CD]",
+    real_int_rate_FR.INR.RINR = "Real interest rate (%) [FR.INR.RINR]", 
+    lending_rate_FR.INR.LEND = "Lending interest rate (%) [FR.INR.LEND]", 
+    
+    # Human Development Index (indirect measure) 
     human_capital_index_HD.HCI.OVRL = "Human capital index (HCI) (scale 0-1) [HD.HCI.OVRL]",
     education_expenditure_SE.XPD.CTOT.ZS = "Current education expenditure, total (% of total expenditure in public institutions) [SE.XPD.CTOT.ZS]",
     life_expectancy_SP.DYN.LE00.IN = "Life expectancy at birth, total (years) [SP.DYN.LE00.IN]",
+    social_contributions_GC.REV.SOCL.ZS = "Social contributions (% of revenue) [GC.REV.SOCL.ZS]", 
+    health_expenditure_SH.XPD.CHEX.PP.CD = "Current health expenditure (% of GDP) [SH.XPD.CHEX.GD.ZS]", 
+    health_expenditure_per_cap = "Current health expenditure per capita, PPP (current international $) [SH.XPD.CHEX.PP.CD]", 
+    electicity_EG.USE.ELEC.KH.PC = "Electric power consumption (kWh per capita) [EG.USE.ELEC.KH.PC]",
+  
+    # Inequality (part of HDI)
     gini_SI.POV.GINI ="Gini index [SI.POV.GINI]",
+    
+    # Unemployment (part of HDI)
     unemployment_SL.UEM.TOTL.NE.ZS = "Unemployment, total (% of total labor force) (national estimate) [SL.UEM.TOTL.NE.ZS]",
     unemployment_youth_SL.UEM.1524.NE.ZS = "Unemployment, youth total (% of total labor force ages 15-24) (national estimate) [SL.UEM.1524.NE.ZS]",
-    export_goods_service_NE.EXP.GNFS.KD.ZG ="Exports of goods and services (annual % growth) [NE.EXP.GNFS.KD.ZG]",
-    merch_export_TX.VAL.MRCH.CD.WT = "Merchandise exports (current US$) [TX.VAL.MRCH.CD.WT]",
-    merch_import_TM.VAL.MRCH.CD.WT = "Merchandise imports (current US$) [TM.VAL.MRCH.CD.WT]",
-    manufacturing_NV.IND.MANF.KD.ZG = "Manufacturing, value added (annual % growth) [NV.IND.MANF.KD.ZG]",
+    
+    # Supply chain indicators 
+    # LPI 
     lpi_LP.LPI.INFR.XQ = "Logistics performance index: Quality of trade and transport-related infrastructure (1=low to 5=high) [LP.LPI.INFR.XQ]",
+    
+    # Freight Rate Indices
     transport_comm_export_TX.VAL.TRAN.ZS.WT ="Transport services (% of commercial service exports) [TX.VAL.TRAN.ZS.WT]",
     transport_comm_import_TM.VAL.TRAN.ZS.WT = "Transport services (% of commercial service imports) [TM.VAL.TRAN.ZS.WT]",
     transport_export_bop_BX.GSR.TRAN.ZS = "Transport services (% of service exports, BoP) [BX.GSR.TRAN.ZS]",
     transport_import_bop_BM.GSR.TRAN.ZS = "Transport services (% of service imports, BoP) [BM.GSR.TRAN.ZS]",
     air_transport_IS.AIR.GOOD.MT.K1 = "Air transport, freight (million ton-km) [IS.AIR.GOOD.MT.K1]",
-    trade_NE.TRD.GNFS.ZS = "Trade (% of GDP) [NE.TRD.GNFS.ZS]",
-    trade_goods_service_BN.GSR.GNFS.CD = "Net trade in goods and services (BoP, current US$) [BN.GSR.GNFS.CD]",
-    net_primary_income_BN.GSR.FCTY.CD = "Net primary income (BoP, current US$) [BN.GSR.FCTY.CD]",
-    household_consumption_expenditure_NE.CON.PRVT.KD.ZG = "Households and NPISHs Final consumption expenditure (annual % growth) [NE.CON.PRVT.KD.ZG]"
+    
+    # BOTH PPI and suuply chain variables 
+    manufacturing_GDP_NV.IND.MANF.ZS = "Manufacturing, value added (% of GDP) [NV.IND.MANF.ZS]", 
+    manufacturing_growth_NV.IND.MANF.KD.ZG = "Manufacturing, value added (annual % growth) [NV.IND.MANF.KD.ZG]",
+    manufacturing_other_NV.MNF.OTHR.ZS.UN = "Other manufacturing (% of value added in manufacturing) [NV.MNF.OTHR.ZS.UN]", 
+    machinery_NV.MNF.MTRN.ZS.UN = "Machinery and transport equipment (% of value added in manufacturing) [NV.MNF.MTRN.ZS.UN]", 
+    manufacturer_exports_TX.VAL.MANF.ZS.UN = "Manufactures exports (% of merchandise exports) [TX.VAL.MANF.ZS.UN]", 
+    manufacturer_imports_TM.VAL.MANF.ZS.UN = "Manufactures imports (% of merchandise imports) [TM.VAL.MANF.ZS.UN]",
+    export_cost_IC.EXP.CSBC.CD = "Cost to export, border compliance (US$) [IC.EXP.CSBC.CD]",
+    import_cost_IC.IMP.CSBC.CD = "Cost to import, border compliance (US$) [IC.IMP.CSBC.CD]",
+    
+    # Producer Price Index (PPI) 
+    exports_GDP_NE.EXP.GNFS.ZS = "Exports of goods and services (% of GDP) [NE.EXP.GNFS.ZS]",
+    exports_growth_NE.EXP.GNFS.KD.ZG ="Exports of goods and services (annual % growth) [NE.EXP.GNFS.KD.ZG]",
+    import_unit_index_TM.UVI.MRCH.XD.WD = "Import unit value index (2015 = 100) [TM.UVI.MRCH.XD.WD]", 
+    import_value_index_TM.VAL.MRCH.XD.WD = "Import value index (2015 = 100) [TM.VAL.MRCH.XD.WD]", 
+    import_volume_index_TM.QTY.MRCH.XD.WD = "Import volume index (2015 = 100) [TM.QTY.MRCH.XD.WD]",
+    oil_rents_NY.GDP.PETR.RT.ZS = "Oil rents (% of GDP) [NY.GDP.PETR.RT.ZS]",
+    ore_exports_TX.VAL.MMTL.ZS.UN = "Ores and metals exports (% of merchandise exports) [TX.VAL.MMTL.ZS.UN]", 
+    ore_imports_TM.VAL.MMTL.ZS.UN = "Ores and metals imports (% of merchandise imports) [TM.VAL.MMTL.ZS.UN]",
+    business_startup_cost_IC.REG.COST.PC.ZS = "Cost of business start-up procedures (% of GNI per capita) [IC.REG.COST.PC.ZS]",
+
+    # Supply Chain Volatility Index 
+    net_trade_goods_service_BN.GSR.GNFS.CD = "Net trade in goods and services (BoP, current US$) [BN.GSR.GNFS.CD]", 
+    net_trade_goods_BN.GSR.MRCH.CD = "Net trade in goods (BoP, current US$) [BN.GSR.MRCH.CD]", 
+    trade_GDP_NE.TRD.GNFS.ZS = "Trade (% of GDP) [NE.TRD.GNFS.ZS]", 
+    trade_services_BG.GSR.NFSV.GD.ZS = "Trade in services (% of GDP) [BG.GSR.NFSV.GD.ZS]", 
+    mechandise_exports_TX.VAL.MRCH.CD.WT = "Merchandise exports (current US$) [TX.VAL.MRCH.CD.WT]",
+    mechandise_imports_TM.VAL.MRCH.CD.WT = "Merchandise imports (current US$) [TM.VAL.MRCH.CD.WT]",
+    mechandise_trade_TG.VAL.TOTL.GD.ZS = "Merchandise trade (% of GDP) [TG.VAL.TOTL.GD.ZS]", 
+    exports_GDP_NE.EXP.GNFS.ZS = "Exports of goods and services (% of GDP) [NE.EXP.GNFS.ZS]", 
+    exports_growth_NE.EXP.GNFS.KD.ZG = "Exports of goods and services (annual % growth) [NE.EXP.GNFS.KD.ZG]", 
+    imports_GDP_NE.IMP.GNFS.ZS = "Imports of goods and services (% of GDP) [NE.IMP.GNFS.ZS]",
+    imports_growth_NE.IMP.GNFS.KD.ZG = "Imports of goods and services (annual % growth) [NE.IMP.GNFS.KD.ZG]",
+    current_account_GDP_BN.CAB.XOKA.GD.ZS = "Current account balance (% of GDP) [BN.CAB.XOKA.GD.ZS]", 
+    current_account_USD_BN.CAB.XOKA.CD = "Current account balance (BoP, current US$) [BN.CAB.XOKA.CD]", 
+    exchange_rate_PX.REX.REER = "Real effective exchange rate index (2010 = 100) [PX.REX.REER]"
   )
 
 data <- data %>%
-  select(country_name, country_code, year, time_code, cpi_FP.CPI.TOTL, net_nat_income_NY.ADJ.NNTY.CD, net_nat_income_percapita_NY.ADJ.NNTY.PC.CD, net_nat_income_percent_NY.ADJ.NNTY.KD.ZG,
-         gdp_percapita_NY.GDP.PCAP.PP.CD, gdp_percapita_growth_NY.GDP.PCAP.KD.ZG, gdp_deflator_NY.GDP.DEFL.ZS, gdp_NY.GDP.MKTP.CD, gdp_constant_NY.GDP.MKTP.KD,
-         business_startup_cost_IC.REG.COST.PC.ZS, good_service_expense_GC.XPN.GSRV.ZS, household_npish_expenditure_percapita_NE.CON.PRVT.PC.KD.ZG,
-         part_time_employment_SL.TLF.PART.ZS, final_consumption_expenditure_NE.CON.TOTL.ZS, net_primary_income_NY.GSR.NFCY.CD, human_capital_index_HD.HCI.OVRL, education_expenditure_SE.XPD.CTOT.ZS,
-         life_expectancy_SP.DYN.LE00.IN, gini_SI.POV.GINI, unemployment_SL.UEM.TOTL.NE.ZS, unemployment_youth_SL.UEM.1524.NE.ZS, export_goods_service_NE.EXP.GNFS.KD.ZG, 
-         merch_export_TX.VAL.MRCH.CD.WT, merch_import_TM.VAL.MRCH.CD.WT, manufacturing_NV.IND.MANF.KD.ZG, lpi_LP.LPI.INFR.XQ, transport_comm_export_TX.VAL.TRAN.ZS.WT, transport_comm_import_TM.VAL.TRAN.ZS.WT,
-         transport_export_bop_BX.GSR.TRAN.ZS, transport_import_bop_BM.GSR.TRAN.ZS, air_transport_IS.AIR.GOOD.MT.K1, trade_NE.TRD.GNFS.ZS, trade_goods_service_BN.GSR.GNFS.CD,
-         net_primary_income_BN.GSR.FCTY.CD, household_consumption_expenditure_NE.CON.PRVT.KD.ZG)
-
+  select(country_name, 
+         country_code, 
+         year, 
+         time_code, 
+         net_nat_income_percapita_NY.ADJ.NNTY.PC.CD, 
+         net_nat_income_NY.ADJ.NNTY.CD, 
+         household_expenditure_GDP, 
+         household_expenditure_per_capita, 
+         household_expenditure_ppp, 
+         gdp_percapita_NY.GDP.PCAP.PP.CD, 
+         gdp_percapita_growth_NY.GDP.PCAP.KD.ZG, 
+         gdp_deflator_NY.GDP.DEFL.ZS, 
+         real_int_rate_FR.INR.RINR, 
+         lending_rate_FR.INR.LEND, 
+         human_capital_index_HD.HCI.OVRL, 
+         education_expenditure_SE.XPD.CTOT.ZS, 
+         life_expectancy_SP.DYN.LE00.IN, 
+         social_contributions_GC.REV.SOCL.ZS, 
+         health_expenditure_SH.XPD.CHEX.PP.CD, 
+         health_expenditure_per_cap, 
+         electicity_EG.USE.ELEC.KH.PC, 
+         gini_SI.POV.GINI, 
+         unemployment_SL.UEM.TOTL.NE.ZS, 
+         unemployment_youth_SL.UEM.1524.NE.ZS, 
+         lpi_LP.LPI.INFR.XQ, 
+         transport_comm_export_TX.VAL.TRAN.ZS.WT, 
+         transport_comm_import_TM.VAL.TRAN.ZS.WT, 
+         transport_export_bop_BX.GSR.TRAN.ZS, 
+         transport_import_bop_BM.GSR.TRAN.ZS, 
+         air_transport_IS.AIR.GOOD.MT.K1, 
+         manufacturing_GDP_NV.IND.MANF.ZS,
+         manufacturing_growth_NV.IND.MANF.KD.ZG, 
+         manufacturing_other_NV.MNF.OTHR.ZS.UN, 
+         machinery_NV.MNF.MTRN.ZS.UN,  
+         manufacturer_exports_TX.VAL.MANF.ZS.UN, 
+         manufacturer_imports_TM.VAL.MANF.ZS.UN, 
+         export_cost_IC.EXP.CSBC.CD, 
+         import_cost_IC.IMP.CSBC.CD, 
+         exports_GDP_NE.EXP.GNFS.ZS, 
+         exports_growth_NE.EXP.GNFS.KD.ZG, 
+         import_unit_index_TM.UVI.MRCH.XD.WD, 
+         import_value_index_TM.VAL.MRCH.XD.WD, 
+         import_volume_index_TM.QTY.MRCH.XD.WD, 
+         oil_rents_NY.GDP.PETR.RT.ZS, 
+         ore_exports_TX.VAL.MMTL.ZS.UN, 
+         ore_imports_TM.VAL.MMTL.ZS.UN, 
+         business_startup_cost_IC.REG.COST.PC.ZS, 
+         net_trade_goods_service_BN.GSR.GNFS.CD, 
+         net_trade_goods_BN.GSR.MRCH.CD, 
+         trade_GDP_NE.TRD.GNFS.ZS, 
+         trade_services_BG.GSR.NFSV.GD.ZS, 
+         mechandise_exports_TX.VAL.MRCH.CD.WT, 
+         mechandise_imports_TM.VAL.MRCH.CD.WT, 
+         mechandise_trade_TG.VAL.TOTL.GD.ZS, 
+         exports_GDP_NE.EXP.GNFS.ZS, 
+         exports_growth_NE.EXP.GNFS.KD.ZG, 
+         imports_GDP_NE.IMP.GNFS.ZS, 
+         imports_growth_NE.IMP.GNFS.KD.ZG, 
+         current_account_GDP_BN.CAB.XOKA.GD.ZS, 
+         current_account_USD_BN.CAB.XOKA.CD, 
+         exchange_rate_PX.REX.REER, 
+  )
+         
 colnames(data)
 
 dim(data)
@@ -109,13 +213,6 @@ for (col in colnames(data)) {
 ###### I'm Confused #####
 # converting from list to dataframe 
 data_mtrx <- as.data.frame(data)
-
-# Storing time in DATE format (USING IF NEEDED)
-data_mtrx$Time_DATE <- paste0(data_mtrx$Time, "-01-01")
-
-data_mtrx$Time_DATE <- (as.Date(data_mtrx$Time_DATE))
-
-str(data_mtrx$Time_DATE)
 
 summary(data_mtrx)
 
